16c354c1d0a47f371fae9741810e25f649b47bb2
el aras <openmason@gmail.com>
2013-02-22 17:10:30 -0800

    unit test stuff added

---
+++ test/basic.js
@@ -0,0 +1,14 @@
+var quid=require('..');
+var assert=require('assert');
+
+// verify basic functions
+describe('basic', function() {
+  before(function(done) {
+    done();
+  });
+  describe('my first unit test', function() {
+    it('should return the correct result', function(done) {
+      done();
+    });
+  });
+});
